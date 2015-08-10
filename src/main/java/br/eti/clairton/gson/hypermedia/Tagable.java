55e1d0fd219467d34c20793a682d940ef2f5999f
Clairton Rodrigo Heinzen <clairton.rodrigo@gmail.com>
2015-08-10 16:26:05 -0300

    Method pluralize in Tagable

--- src/main/java/br/eti/clairton/gson/hypermedia/Tagable.java
+++ src/main/java/br/eti/clairton/gson/hypermedia/Tagable.java
@@ -1,7 +1,5 @@
 package br.eti.clairton.gson.hypermedia;
 
-import java.util.Collection;
-
 import br.eti.clairton.inflector.Inflector;
 
 public class Tagable<T> extends br.eti.clairton.jpa.serializer.Tagable<T> {
@@ -12,12 +10,9 @@ public class Tagable<T> extends br.eti.clairton.jpa.serializer.Tagable<T> {
 		super();
 		this.inflector = inflector;
 	}
-
+	
 	@Override
-	public String getRootTagCollection(final Collection<T> collection) {
-		final T src = getFirst(collection);
-		final String tag = getRootTag(src);
-		final String collectionTag = inflector.pluralize(tag);
-		return collectionTag;
+	protected String pluralize(final String tag){
+		return inflector.pluralize(tag);
 	}
 }
